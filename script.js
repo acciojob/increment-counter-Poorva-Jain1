2281373edb2a45afa1359da60eca79083fdfab3e
Poorva-Jain1 <poorvajainnagod@gmail.com>
2025-12-10 23:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const counter = document.getElementById('counter');
+const btn = document.getElementById('incrementBtn');
+btn.addEventListener('click', function () {
+	let currentValue = Number(counter.textContent);
+
+	alert("Current value: " + currentValue);
+
+	counter.textContent = currentValue + 1;
+});
